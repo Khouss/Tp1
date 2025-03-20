589ac43a22048fdd9907677cc405a08d157c9cd0
Khouss <oussamakhedif82@gmail.com>
2025-03-20 02:15:03 +0000

    Premier commit

---
+++ src/main/java/dao/DaoImpl.java
@@ -0,0 +1,13 @@
+package dao;
+
+public class DaoImpl implements IDao {
+
+
+
+    @Override
+    public double getData() {
+        System.out.println("Version base de données");
+        double temp=23;
+        return temp;
+    }
+}
---
+++ src/main/java/pres/PresentationV1.java
@@ -0,0 +1,14 @@
+package pres;
+import dao.DaoImpl;
+import ext.DaoImplV2;
+import metier.MetierImpl;
+
+public class PresentationV1 {
+    public static void main(String[] args){
+        //DaoImpl d = new DaoImpl();
+       DaoImplV2 d = new DaoImplV2();
+        MetierImpl metier = new MetierImpl(d);
+        //metier.setDao(d); //injection via le setter
+        System.out.println("Res "+metier.calcul());
+    }
+}
