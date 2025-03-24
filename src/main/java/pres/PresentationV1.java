b602aa14957455bf37d3ec70bdb7be507a90fca9
Khouss <oussamakhedif82@gmail.com>
2025-03-24 01:16:37 +0000

    3 commit

--- src/main/java/pres/PresentationV1.java
+++ src/main/java/pres/PresentationV1.java
@@ -7,7 +7,7 @@ public class PresentationV1 {
     public static void main(String[] args){
         //DaoImpl d = new DaoImpl();
        DaoImplV2 d = new DaoImplV2();
-        MetierImpl metier = new MetierImpl(d);
+        MetierImpl metier = new MetierImpl(d); //via constructeur
         //metier.setDao(d); //injection via le setter
         System.out.println("Res "+metier.calcul());
     }
